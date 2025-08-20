9568516dbf7a085416bb2390bb909da43d8e1822
abdullahjr11 <abdullah11jabvaid@gmail.com>
2025-08-20 23:10:33 +0500

    Challenges updated

--- Fundamentals part-2/script.js
+++ Fundamentals part-2/script.js
@@ -57,38 +57,38 @@ console.log(age2);
 // }
 // console.log(juiceFruits(2, 3));
 
-//Challenge-1 :
+// Challenge-1 :
 
-// const calcAverage = (score1,score2,score3) => (score1+score2+score3)/3;
-// const scoreDolphins = calcAverage(44,23,71);
-// const scoreKoalas = calcAverage(65,54,49);
-// // checkWinner(scoreDolphins,scoreKoalas)
-// function checkWinner(avgDolphins,avgKoalas){
-//     if (avgDolphins > avgKoalas*2){
-//         console.log(`Dolphins win ${avgDolphins} vs ${avgKoalas})`);
-//     }else if (avgKoalas > avgDolphins*2){
-//         console.log(`Koalas win (${avgkoalas} vs ${avgDolphins})`);
-//     }else{
-//         console.log('NO team wins');
-//     }
-// }
-// console.log(scoreDolphins,scoreKoalas)
-// checkWinner(scoreDolphins,scoreKoalas);
+const calcAverage = (score1,score2,score3) => (score1+score2+score3)/3;
+const scoreDolphins = calcAverage(44,23,71);
+const scoreKoalas = calcAverage(65,54,49);
+// checkWinner(scoreDolphins,scoreKoalas)
+function checkWinner(avgDolphins,avgKoalas){
+    if (avgDolphins > avgKoalas*2){
+        console.log(`Dolphins win ${avgDolphins} vs ${avgKoalas})`);
+    }else if (avgKoalas > avgDolphins*2){
+        console.log(`Koalas win (${avgkoalas} vs ${avgDolphins})`);
+    }else{
+        console.log('NO team wins');
+    }
+}
+console.log(scoreDolphins,scoreKoalas)
+checkWinner(scoreDolphins,scoreKoalas);
 
-//challenge-2 :
+// challenge-2 :
 
-// function calcTip(bills) {
-//   if (bills >= 50 && bills <= 300){
-//     return bills*0.15;
-//   }else{
-//     return bills*0.20;
-//   }
-// }
-// const bills=[125,255,244];
-// const tips=[calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
+function calcTip(bills) {
+  if (bills >= 50 && bills <= 300){
+    return bills*0.15;
+  }else{
+    return bills*0.20;
+  }
+}
+const bills=[125,255,244];
+const tiips=[calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
 
-// const totals = (tips[0],tips[1],tips[2]);
-// console.log(`Total value is ${totals} which you have to pay. Danke!`);
+const ttotals = (tiips[0],tiips[1],tiips[2]);
+console.log(`Total value is ${ttotals} which you have to pay. Danke!`);
 
 //challenge - 3:
 
